7057bec03defc5f46602d6d8d5b7a7851320351a
Cem Usta <nooneelse@gmail.com>
2020-06-01 15:07:36 +0200

    tests for controller added

---
+++ test/controllers/flightController.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest')
+beforeEach(() => {
+  jest.resetModules()
+})
+
+it('should return flights array', async function (done) {
+  jest.mock('../../src/services/flightService', () => ({
+    consolidateFlights: jest.fn(async () => {
+      // return { status: 200, text: mockText }
+      // return new Promise(resolve => setTimeout(() => resolve({ flights: [] }), 0))
+      return []
+    })
+  }))
+  const app = require('../../src/app')
+  const res = await request(app)
+    .get('/api/flights')
+
+  expect(res).toHaveProperty('status', 200)
+  expect(res).toHaveProperty('text', '{"flights":[]}')
+  done()
+})
+
+it('should return 500 on error', async function (done) {
+  jest.mock('../../src/services/flightService', () => ({
+    consolidateFlights: jest.fn(async () => {
+      throw new Error('Mock error')
+    })
+  }))
+  const app = require('../../src/app')
+  const res = await request(app)
+    .get('/api/flights')
+
+  expect(res).toHaveProperty('status', 500)
+  expect(res).toHaveProperty('text', '{"status":"error","message":"Mock error"}')
+  done()
+})
